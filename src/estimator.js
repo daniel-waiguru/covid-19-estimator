0597f7a06abe03a69c7bd1a03e4da43dc57b0033
Daniel Waiguru <52042764+DanielWaiguru91@users.noreply.github.com>
2020-04-14 14:52:01 +0300

    Revert "First challenge"

--- src/estimator.js
+++ src/estimator.js
@@ -1,16 +1,3 @@
 const covid19ImpactEstimator = (data) => data;
-{    
-    const input = data;
-    return {
-        data: input,
-        impact:{
-            currentlyInfected: reportedCases * 10,
-            infectionsByRequestedTime: currentlyInfected * 1024
-        },
-        severeImpact:{
-            currentlyInfected: reportedCases * 50,
-            infectionsByRequestedTime: currentlyInfected * 1024
-        }
-    };
-}
+
 export default covid19ImpactEstimator;
